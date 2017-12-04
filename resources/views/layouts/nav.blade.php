4e144cfefe7e6da80e96fecd523284ce753a6a0c
Owden Godson <owdeng@gmail.com>
2017-12-04 15:04:00 +0300

    removed some typos

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -36,7 +36,7 @@
                         <li> <a href="{{ url('users') }}" class=""><i class=" fa-fw">1</i><span class="hide-menu">Users</span></a> </li>
                         @role('admin') <li> <a href="{{url('roles')}}"><i class=" fa-fw">2</i><span class="hide-menu">User Types</span></a> </li>
 
-                        <li> <a href="#"><i class=" fa-fw">3</i><span class="hide-menu">Permissinos</span></a> </li>
+                        <li> <a href="#"><i class=" fa-fw">3</i><span class="hide-menu">Permissions</span></a> </li>
                         @endrole
                     </ul>
                 </li>
