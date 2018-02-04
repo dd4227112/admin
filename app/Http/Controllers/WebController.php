0385a477120f1e58781beac09ca89a0de4319af1
Ephraim Swilla <swillae1@gmail.com>
2018-02-05 00:12:03 +0300

    let me track daily log requests now for starting

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -7,71 +7,71 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Auth;
 use DB;
-class WebController extends Controller
-{
-    public  $path='storage'.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'';
-    
+
+class WebController extends Controller {
+
+    public $path = 'storage' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . '';
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    
-    public function index($pg=null,$sub=null)
-    {
+    public function index($pg = null, $sub = null) {
 
-        if (Auth::check()){
-             $page=$pg==null ? 'home' : $pg;
-         }else{
-             $page='login';
-         }
-         $data= ($pg ==null || in_array($page, array('login')))?'':$this->$pg($sub);
-         $this->data['data']=$data;
-         $this->data['page']=$page;
-       return view(strtolower($page),$this->data);
+        if (Auth::check()) {
+            $page = $pg == null ? 'home' : $pg;
+        } else {
+            $page = 'login';
+        }
+        $data = ($pg == null || in_array($page, array('login'))) ? '' : $this->$pg($sub);
+        $this->data['data'] = $data;
+        $this->data['page'] = $page;
+        return view(strtolower($page), $this->data);
     }
 
-    public function users(){
+    public function users() {
         return 'true';
     }
 
-    public function tag($pg=null,$sub=null){
+    public function tag($pg = null, $sub = null) {
         return $this->$pg($sub);
     }
 
-   
-    public function logs(){
+    public function logs() {
         return scandir($this->path);
     }
 
-    public function readLog($log_path){
-        return file_get_contents($this->path.'/'.$log_path);
+    public function readLog($log_path) {
+        return file_get_contents($this->path . '/' . $log_path);
     }
 
-    public function deleteLog($log_path){
-      return  is_file($this->path.'/'.$log_path) ?
-         unlink($this->path.'/'.$log_path) : 0;
+    public function deleteLog($log_path) {
+        return is_file($this->path . '/' . $log_path) ?
+                unlink($this->path . '/' . $log_path) : 0;
     }
 
-    public function logsummary(){
-     // DB::statement("select admin.join_all('log')");
-       return DB::select('select count(*) as total_logs,"schema_name"::text from admin.all_log group by "schema_name"::text order by count(*)');
+    public function logsummary() {
+        // DB::statement("select admin.join_all('log')");
+        $this->data['start'] = request()->segment(2);
+        $this->data['end'] = request()->segment(3);
+
+        return DB::select('select count(*) as total_logs,"schema_name"::text from admin.all_log group by "schema_name"::text order by count(*)');
     }
-    
+
     public function analyse($schema) {
-        $this->data['request_by_user']=DB::select('select count(*),"schema_name"::text,"user" from admin.all_log where "schema_name"::text=\''.$schema.'\' and created_at::date=\''.date('Y-m-d').'\' group by "schema_name"::text,"user"');
-        $this->data['request_group']=DB::select('select count(*),"user_agent" from admin.all_log where "schema_name"::text=\''.$schema.'\' group by "user_agent"');
-        $this->data['total']=\collect(DB::select("select count(*) from ".$schema.".log where created_at::date='".date('Y-m-d')."'"))->first();
-        $this->data['schema']=$schema;
-  
+        $this->data['request_by_user'] = DB::select('select count(*),"schema_name"::text,"user" from admin.all_log where "schema_name"::text=\'' . $schema . '\' and created_at::date=\'' . date('Y-m-d') . '\' group by "schema_name"::text,"user"');
+        $this->data['request_group'] = DB::select('select count(*),"user_agent" from admin.all_log where "schema_name"::text=\'' . $schema . '\' group by "user_agent"');
+        $this->data['total'] = \collect(DB::select("select count(*) from " . $schema . ".log where created_at::date='" . date('Y-m-d') . "'"))->first();
+        $this->data['schema'] = $schema;
     }
+
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
@@ -81,8 +81,7 @@ class WebController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
@@ -92,8 +91,7 @@ class WebController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
@@ -103,8 +101,7 @@ class WebController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
@@ -115,8 +112,7 @@ class WebController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
@@ -126,8 +122,8 @@ class WebController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         //
     }
+
 }
--- resources/views/logsummary.blade.php
+++ resources/views/logsummary.blade.php
@@ -1,46 +1,88 @@
 @extends('layouts.app')
 @section('content')
 
-                  <div class="row">
-                    <div class="white-box">
-                    <h5>Request made by each School</h5>
-                    <div class="table-responsive"> 
-                                <table id="example23" class="display nowrap table color-table success-table" cellspacing="0" >
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>School Name</th>
-                                            <th>Total Requests</th>
-                                            <th>Parent</th>
-                                            <th>Teachers</th>
-                                            <th>Others</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                   <?php $i=1;
-                                 
-                                   foreach ($data as $key => $value) {
-                                 $parents=\DB::table('admin.all_log')->where('user','Parent')->count();
-                                 $teachers=\DB::table('admin.all_log')->where('user','Teacher')->count();
-                                 $others=($value->total_logs-($parents+$teachers));
-                                      ?>
-                                        <tr>
-                                            <td><?=$i?></td>
-                                            <td><?=$value->schema_name?></td>
-                                                         <td>
-                                            <a href="<?=url('readRequest/').'/'.$value->total_logs?>"><?=$value->total_logs?></a></td>          
-                                           <td><?=$parents?></td> 
-                                            <td><?=$teachers?></td> 
-                                            <td><?=$others?></td> 
-                                 
-                                            <td><a href="<?=url('analyse/').'/'.$value->schema_name?>" class="btn btn-sm btn-info">Analyse</a></td>
-                                        </tr>
-                                        <?php $i++; } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            </div>
-                </div>
+<div class="row">
+    <div class="white-box">
+        <h5>Total Request made by Date</h5>
+        <script type="text/javascript">
+            $(function () {
+                $('#container').highcharts({
+                    data: {
+                        table: 'datatable'
+                    },
+                    series: [{
+                            name: '',
+                            colorByPoint: true}
+                    ],
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: 'Log Requests'
+                    },
+                    yAxis: {
+                        allowDecimals: false,
+                        title: {
+                            text: 'Average'
+                        }
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    plotOptions: {
+                        series: {
+                            borderWidth: 0,
+                            dataLabels: {
+                                enabled: true,
+                                format: ''
+                            }
+                        }
+                    },
+                    tooltip: {
+                        formatter: function () {
+                            return '<b>' + this.series.name + '</b><br/>' +
+                                    this.point.y + ' ' + this.point.name.toUpperCase();
+                        }
+                    }
+                });
+            });
+        </script>
+
+        <script src="<?= url('public/js/highcharts.js') ?>"></script>
+        <script src="<?= url('public/js/exporting.js') ?>"></script>
+        <script src="<?= url('public/js/data.js') ?>"></script>
+
+
+        <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        <div class="table-responsive"> 
+            <table id="datatable" style="display:none" >
+                <thead>
+                    <tr>
+                        <th>Count</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $i = 1;
+                    $from_date = date('Y-m-d', strtotime($start)) == '1970-01-01' ? date('Y-m-d', time() - 60 * 60 * 24 * 30) : date('Y-m-d', strtotime($start));
+                    $end_date = date('Y-m-d', strtotime($end)) == '1970-01-01' ? date('Y-m-d') : date('Y-m-d', strtotime($end));
+                    $logs = \DB::select("select count(*),created_at::date from admin.all_log where created_at::date <= '" . $end_date . "' and created_at::date>= '" . $from_date . "' group by created_at::date order by created_at desc");
+                    foreach ($logs as $value) {
+                        ?>
+                        <tr>
+                             <td><?= $value->created_at ?></td>
+                            <td><?= $value->count ?></td>
+                           
+                        </tr>
+                        <?php
+                        $i++;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @include('layouts.datatable')
 @endsection
