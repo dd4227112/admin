60fb974471bb4ab0a3ee35d2f2d0642ba34352aa
Ephraim Swilla <swillae1@gmail.com>
2018-02-10 16:29:10 +0300

    double SMS sending rate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel {
 //check if there is any email then send
 //$this->testCrone();
 
-            $messages = DB::select('select * from public.all_sms order by random() limit 40');
+            $messages = DB::select('select * from public.all_sms order by random() limit 25');
             if (!empty($messages)) {
                 foreach ($messages as $sms) {
 
