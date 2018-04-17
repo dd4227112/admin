8ae6d801a786ec51c9bc9dcb4a9eb6110701e0f7
Ephraim Swilla <swillae1@gmail.com>
2018-04-17 15:43:09 +0300

    user module

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,18 +7,17 @@ use Illuminate\Http\Request;
 use App\User;
 use DB;
 
-class UsersController extends Controller
-{
+class UsersController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         $users = User::orderBy('id', 'DESC')->paginate(6);
         return view('users.index', compact('users'))
-            ->with('i', ($request->input('page', 1) - 1) * 5);
+                        ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -26,8 +25,7 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         $roles = Role::get();
         $users = User::get();
         return view('users.create', compact('roles', 'users'));
@@ -40,26 +38,38 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-
-
+    public function store(Request $request) {
         $this->validate($request, [
             'firstname' => 'required|max:255',
             'lastname' => 'required|max:255',
             'phone' => 'required|max:255|unique:users',
             'email' => 'required|max:255|unique:users'
-
         ]);
-        $user = new User(array_merge($request->all(),['password'=> bcrypt(request('email'))]));
+        $user = new User(array_merge($request->all(), ['password' => bcrypt(request('email'))]));
         $user->save();
-
+        $this->sendEmailAndSms($request);
         foreach ($request->input('roles') as $key => $value) {
             $user->attachRole($value);
         }
 
         return redirect()->route('users.index')
-            ->with('success', 'User ' . $request['firstname'] . ' created successfully');
+                        ->with('success', 'User ' . $request['firstname'] . ' created successfully');
+    }
+    public function sendEmailAndSms($request) {
+        $message = 'Hello ' . $request->name . ' You have been added in ShuleSoft Administration panel. You can login for Administration of schools with username ' . $request->email . ' and password ' . $request->email;
+        \DB::table('public.sms')->insert([
+            'body' => $message,
+            'user_id' => 1,
+            'phone_number' => $request->phone,
+            'table' => 'setting'
+        ]);
+        \DB::table('public.email')->insert([
+            'body' => $message,
+            'subject' => 'ShuleSoft Administration Credentials',
+            'user_id' => 1,
+            'email' => $request->email,
+            'table' => 'setting'
+        ]); 
     }
 
     /**
@@ -69,12 +79,11 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         $user = User::find($id);
         $userRoles = Role::join("role_user", "role_user.role_id", "=", "roles.id")
-            ->where("role_user.user_id", $id)
-            ->get();
+                ->where("role_user.user_id", $id)
+                ->get();
 
         return view('users.show', compact('user', 'userRoles'));
     }
@@ -86,12 +95,11 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         $user = User::find($id);
         $role = Role::get();
         $userRoles = DB::table("role_user")->where("role_user.user_id", $id)
-            ->pluck('role_user.role_id', 'role_user.role_id')->toArray();
+                        ->pluck('role_user.role_id', 'role_user.role_id')->toArray();
 
         return view('users.edit', compact('user', 'role', 'userRoles'));
     }
@@ -104,8 +112,7 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         $this->validate($request, [
             'firstname' => 'required|max:255',
             'lastname' => 'required|max:255',
@@ -121,14 +128,14 @@ class UsersController extends Controller
         $user->save();
 
         DB::table("role_user")->where("role_user.user_id", $id)
-            ->delete();
+                ->delete();
 
         foreach ($request->input('role') as $key => $value) {
             $user->attachRole($value);
         }
 
         return redirect()->route('users.index')
-            ->with('success', 'User ' . request('firstname') . ' ' . request('lastname') . ' updated successfully');
+                        ->with('success', 'User ' . request('firstname') . ' ' . request('lastname') . ' updated successfully');
     }
 
     /**
@@ -138,16 +145,16 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         DB::table("users")->where('id', $id)->delete();
         return redirect()->route('users.index')
-            ->with('success', 'User deleted successfully');
+                        ->with('success', 'User deleted successfully');
     }
-    
+
     public function management() {
-        $sql='SELECT * FROM public.crosstab(\'select "schema_name"::text,"table",count(*) from admin.all_users where status=1  group by "schema_name"::text,"table" order by 1,2\', \'select distinct "table"::text from admin.all_users order by 1\') AS final_result("schema_name" text,"parent" text,"setting" text, "student" text, "teacher" text, "user" text)';
-        $this->data['users']=DB::select($sql);
+        $sql = 'SELECT * FROM public.crosstab(\'select "schema_name"::text,"table",count(*) from admin.all_users where status=1  group by "schema_name"::text,"table" order by 1,2\', \'select distinct "table"::text from admin.all_users order by 1\') AS final_result("schema_name" text,"parent" text,"setting" text, "student" text, "teacher" text, "user" text)';
+        $this->data['users'] = DB::select($sql);
         return view('users.school_users', $this->data);
     }
+
 }
