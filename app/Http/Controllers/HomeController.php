61e7db1fb28b251d53bba1e5a89c7b2f8f6c6b9c
Ephraim Swilla <swillae1@gmail.com>
2018-05-09 21:22:41 +0300

    email

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller {
 
