5b91cc092474b024197a13fdff26d7ca5f12d4d2
Ephraim Swilla <swillae1@gmail.com>
2019-02-04 14:44:17 +0300

    invoice

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -120,8 +120,8 @@ class Kernel extends ConsoleKernel {
 
     public function checkSchedule() {
         $schedules = DB::table('admin.all_reminders')->get();
-       // $current_time = date('H:i', strtotime(date('H:i')) + 60 * 60 * 3); // plus +3 GMT hours to match with Tanzania time
-        $current_time = date('H:i');
+       $current_time = date('H:i', strtotime(date('H:i')) + 60 * 60 * 3); // plus +3 GMT hours to match with Tanzania time
+     //   $current_time = date('H:i');
         foreach ($schedules as $schedule) {
             if (strlen($schedule->days) > 4) {
                 $days = explode(',', $schedule->days);
