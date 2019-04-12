90656973de5d45933f7808dcfd20b25146fcb331
Ephraim Swilla <swillae1@gmail.com>
2019-04-12 11:54:03 +0300

    information

--- resources/views/message/add_updates.blade.php
+++ resources/views/message/add_updates.blade.php
@@ -54,26 +54,12 @@
     </form>
 
 </div>
-<script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
-<script type="text/javascript">
-
-    wywg = function () {
-            tinymce.init({
-                selector: "textarea",
-                //theme: "modern",
-                height: 300,
-                plugins: [
-                    "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-                    "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-                    "save table contextmenu directionality emoticons template paste textcolor"
-                ],
-                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
-
+<script src="<?= $root ?>plugins/bower_components/html5-editor/wysihtml5-0.3.0.js"></script>
+<script src="<?= $root ?>plugins/bower_components/html5-editor/bootstrap-wysihtml5.js"></script>
+<script>
+$(document).ready(function () {
+$('.textarea_editor').wysihtml5();
             });
-        
-    }
-
-    $(document).ready(wywg);
     </script>
 <script src="<?= $root ?>js/clipboard.min.js"></script>
 <script>
