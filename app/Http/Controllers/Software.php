12e88fec808d2d329f5e35e57774a670c4be1638
Ephraim Swilla <swillae1@gmail.com>
2019-08-14 14:37:04 +0300

    software controller auth

--- app/Http/Controllers/Software.php
+++ app/Http/Controllers/Software.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use DB;
 
 class Software extends Controller {
+    
+     public function __construct() {
+        $this->middleware('auth');
+    }
 
     /**
      * Display a listing of the resource.
