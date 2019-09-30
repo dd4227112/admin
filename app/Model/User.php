736b9350b589daeab688165ad6ff55d41257047b
Ephraim Swilla <swillae1@gmail.com>
2019-09-30 12:52:57 +0300

    users add option

--- app/Model/User.php
+++ app/Model/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable {
     }
 
     public function role() {
-        return $this->belongsTo('App\Model\Role');
+        return $this->belongsTo('App\Model\Role')->withDefault(['display_name'=>'unknown']);
     }
 
 }
