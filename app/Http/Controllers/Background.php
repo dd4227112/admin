c12f7f594a3cb35627f89d50af96f110d64c74b3
Ephraim Swilla <swillae1@gmail.com>
2019-11-19 10:39:06 +0300

    background

--- app/Http/Controllers/Background.php
+++ app/Http/Controllers/Background.php
@@ -7,7 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use DB;
 
-class BackgroundController extends Controller {
+class Background extends Controller {
 
     /**
      * Create a new controller instance.
