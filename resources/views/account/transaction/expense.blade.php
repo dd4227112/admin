8a1ea08bdde1d2d3d142873ccdb65558202e82df
Ephraim Swilla <swillae1@gmail.com>
2019-12-27 00:11:33 +0300

    upgrade account module expense view

--- resources/views/account/transaction/expense.blade.php
+++ resources/views/account/transaction/expense.blade.php
@@ -192,7 +192,7 @@
 
                                                         echo money($total_amount);
                                                     } else {
-                                                        $total_amount = count($expense->expenses()->get()) > 0 ? money($expense->expenses()->sum('amount')) : '';
+                                                        $total_amount = count($expense->expenses()->get()) > 0 ? money($expense->expenses()->whereDate('date','>=',$from_date)->whereDate('date','<=',$to_date)->sum('amount')) : '';
                                                         echo $total_amount;
                                                     }
 
