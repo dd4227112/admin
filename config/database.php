a3de8fe1c16bcfa12da4b151f87d0d5fa575d7c5
Ephraim Swilla <swillae1@gmail.com>
2020-07-01 21:43:12 +0300

    database

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'host' => '127.0.0.1',
             'port' =>5432,
 
-            'database' => 'Shulesoft',
+            'database' => 'back22may',
             'username' => 'postgres',
-            'password' =>'kiyogoma',
+            'password' =>'newpassword',
             'charset' => 'utf8',
             'prefix' => '',
             'schema' =>'admin',
