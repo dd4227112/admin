380c1f33ad48575a5128d26c8dbeb461e62c7036
Albogast <albogastkiyogoma@gmail.com>
2020-08-03 16:35:48 +0300

    view

--- resources/views/analyse/sales.blade.php
+++ resources/views/analyse/sales.blade.php
@@ -278,54 +278,7 @@ where extract(year from a.created_at)=' . $year . '  group by month order by mon
             <!-- Google Chart end-->
             <!-- Recent Order table start -->
             <div class="row">
-                <div class="col-xl-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>SALES ACTIVITIES</h5>
-                        </div>
-                        <div class="card-block">
-                            <div class="table-responsive">
-                                <div class="dt-responsive table-responsive">
-                                    <table id="res-config" class="table table-bordered w-100 dataTable">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Task</th>
-                                                <th>Task Type</th>
-                                                <th>Date</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $t = '-' . $days . ' days';
-                                            $at = date('Y-m-d H:i:s', strtotime($t));
-                                            $i = 1;
-                                            $activities = $activities = DB::select("select a.id,d.username, a.activity,a.created_at,b.name as task_name, c.firstname||' '||c.lastname as user_name from admin.tasks a join admin.task_types b on b.id=a.task_type_id join admin.users c on c.id=a.user_id join admin.clients d on d.id=a.client_id WHERE  a.task_type_id in (select id from admin.task_types where department=2) and " . $where);
-
-                                            foreach ($activities as $activity) {
-                                                ?>
-
-                                                <tr>
-                                                    <td class="pro-name"><?= $activity->task_name ?></td>
-                                                    <td class="img-pro"><?= $activity->user_name ?></td>
-                                                    <td><a href="<?= url('customer/activity/show/' . $activity->id) ?>"><?= $activity->username ?></a></td>
-                                                    <td> <label class="text-danger"><?= $activity->created_at ?></td>
-                                                </tr>
-                                            <?php } ?>
-                                            <?php
-                                            //
-                                            ?>
-                                        </tbody>
-                                    </table>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Monthly Growth Chart end-->
-                <!-- Google Chart start-->
-                <div class="col-xl-4">
+                <div class="col-xl-12">
                     <div class="card">
                         <div class="card-header">
                             <h5>Sales Distribution Activities</h5>
