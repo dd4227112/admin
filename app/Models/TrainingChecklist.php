6a338b4552040f89d439dc4027398ae91469c865
Ephraim Swilla <swillae1@gmail.com>
2020-08-13 09:15:38 +0300

    add tasks management

---
+++ app/Models/TrainingChecklist.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrainingChecklist extends Model {
+
+    /**
+     * Generated
+     */
+    protected $table = 'admin.training_checklist';
+    protected $fillable = ['id', 'title', 'position', 'page_section', 'key_columns', 'key_table', 'created_by', 'training_section_id', 'created_at', 'updated_at'];
+
+    public function trainingSection() {
+        return $this->belongsTo(\App\Model\TrainingSection::class);
+    }
+
+    public function training() {
+        return $this->hasMany(\App\Model\Training::class);
+    }
+
+}
