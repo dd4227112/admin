a574bd3a5bcaa9e3ab6b79aa1ce89d621e010820
Ephraim Swilla <ephraim@shulesoft.com>
2020-09-11 15:52:03 +0300

    view of customer setup statistics

--- resources/views/analyse/accounts.blade.php
+++ resources/views/analyse/accounts.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 @section('content')
-<?php $root = url('/') . '/public/' ?>
+<?php $root = url('/') . '/public/';
+$root = url('/') . '/public/';
+$page = request()->segment(3);
+$today = 0;
+
+if ((int) $page == 1 || $page == 'null' || (int) $page == 0) {
+    //current day
+    $where = '  a.created_at::date=CURRENT_DATE';
+    $today = 1;
+    $year = date('Y');
+} else {
+    $start_date = date('Y-m-d', strtotime(request('start')));
+    $end_date = date('Y-m-d', strtotime(request('end')));
+    $year = date('Y', strtotime(request('start')));
+    $where = "  a.created_at::date >='" . $start_date . "' AND a.created_at::date <='" . $end_date . "'";
+}
+ ?>
 <div class="page-wrapper">
     <div class="page-header">
         <div class="page-header-title">
@@ -22,6 +38,120 @@
     </div>
     <?php if (can_access('manage_users')) { ?>
         <div class="page-body">
+                <div class="row">
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4 text-right">
+                <select class="form-control" id="check_custom_date">
+                    <option value="today" <?= $today == 1 ? 'selected' : '' ?>>Today</option>
+                    <option value="custom"  <?= $today == 0 ? 'selected' : '' ?>>Custom</option>
+                </select>
+
+            </div>
+        </div>
+        <div class="row" style="display: none" id="show_date">
+
+            <div class="col-lg-4"></div>
+            <div class="col-lg-8 text-right">
+                <h4 class="sub-title">Date Time Picker</h4>
+                <div class="input-daterange input-group" id="datepicker">
+                    <input type="date" class="input-sm form-control calendar" name="start" id="start_date">
+                    <span class="input-group-addon">to</span>
+                    <input type="date" class="input-sm form-control" name="end" id="end_date">
+                    <input type="submit" class="input-sm btn btn-sm btn-success" id="search_custom"/>
+                </div>
+            </div>
+
+        </div>
+             <div class="row">
+                <!-- Documents card start -->
+                <div class="col-md-6 col-xl-3">
+                    <div class="card client-blocks dark-primary-border">
+                        <div class="card-block">
+                            <h5> Invoice Issued</h5>
+                            <ul>
+                                <li>
+                                    <i class="icofont icofont-list"></i>
+                                </li>
+                                <li class="text-right">
+                                     <?php
+                                     $total_school=\collect(DB::select('select count(*) from admin.all_setting a WHERE  ' . $where))->first()->count;
+
+                                $invoices_sent = \collect(DB::select('select count(*) from admin.invoices_sent a WHERE  ' . $where))->first()->count;
+                                ?>
+                                    <?php echo $invoices_sent; ?>
+                                </li>
+                                <span class="small"><?= round($invoices_sent * 100 / ((int)$total_school==0 ? 1:$total_school), 1) ?>% issued</span>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Documents card end -->
+                <!-- New clients card start -->
+                <div class="col-md-6 col-xl-3">
+                    <div class="card client-blocks warning-border">
+                        <div class="card-block">
+                            <h5>Total Invoice Paid</h5>
+                            <ul>
+                                <li>
+                                    <i class="ti-layout text-warning"></i>
+                                </li>
+                                <li class="text-right text-warning">
+                                         <?php
+                                $all_setting = \collect(DB::select('select count(*) from admin.all_setting  '))->first()->count;
+                                ?>
+                                    <?php echo $all_setting; ?>
+                                </li>
+                                <span class="small"><?= ' percentage (' . round(43 * 100 / ((int) 434 == 0 ? 1 : 4), 1) ?>%)</span>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- New clients card end -->
+                <!-- New files card start -->
+                <div class="col-md-6 col-xl-3">
+                    <div class="card client-blocks success-border">
+                        <div class="card-block">
+                            <h5>Our Clients</h5>
+                            <ul>
+                                <li>
+                                    <i class="icofont icofont-users text-success"></i>
+                                </li>
+                                <li class="text-right text-success">
+                                     <?php
+                                $all_setting = \collect(DB::select('select count(*) from admin.all_setting  '))->first()->count;
+                                ?>
+                                    <?php echo $all_setting; ?>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- New files card end -->
+                <!-- Open Project card start -->
+                <div class="col-md-6 col-xl-3">
+                    <div class="card client-blocks">
+                        <div class="card-block">
+                            <h5>Clients Pay</h5>
+                            <ul>
+                                <li>
+                                    <i class="icofont icofont-ui-folder text-primary"></i>
+                                </li>
+                                <li class="text-right text-primary">
+                                    <?php echo 43; ?>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- Open Project card end -->
+            </div>
+            <?php 
+if(in_array(Auth::user()->id, [2,3,7])){ 
+            ?>
             <div class="row">
                 <!-- counter-card-1 start-->
                 <div class="col-md-12 col-xl-4">
@@ -100,7 +230,7 @@
                 <!-- Monthly Growth Chart start-->
     
             </div>
-
+<?php } ?>
                 <div class="row">
                     <div class="col-xl-6">
                           
@@ -118,7 +248,16 @@
                                     <div class="card-block">
 
                                         <?php
-                                 $sql_2 = "select sum(amount) as count, extract(month from created_at) as month from admin.payments a   where extract(year from created_at)=".date('Y')."  group by month order by month asc ";
+                                        
+
+                                 $sql_2 = "select sum(count) as count, month from (
+
+                                 select sum(amount) as count, extract(month from created_at) as month from admin.payments a   where extract(year from created_at)=".date('Y')." group by month
+                                 UNION ALL
+                                 select sum(amount) as count, extract(month from created_at) as month from admin.revenues a   where extract(year from created_at)=".date('Y')." group by month
+
+
+                             ) a group by month order by month asc ";
 
                                echo $insight->createChartBySql($sql_2, 'month', 'Payments Collection Per Month', 'line', false);
                                         ?>
@@ -137,20 +276,20 @@
                             <h5>Budget Distribution</h5>
                         </div>
                         <div class="card-block">
-                            <?php
-                            $rations = DB::select('select a.name, sum(b.amount) from admin.budget_ratios a join admin.payments_budget_ratios b on a.id=b.budget_ratio_id  group by a.name');
-                            $widget=['primary','success','inverse'];
-                            foreach ($rations as $ratio) {
+                      <!--       <?php
+                            //$rations = DB::select('select a.name, sum(b.amount) from admin.budget_ratios a join admin.payments_budget_ratios b on a.id=b.budget_ratio_id  group by a.name');
+                           // $widget=['primary','success','inverse'];
+                           // foreach ($rations as $ratio) {
                                 ?>
-                            <div class="card bg-<?= $widget[rand(0, 2)]?> large-widget-card">
+                            <div class="card bg-<?php // $widget[rand(0, 2)]?> large-widget-card">
                                     <div class="card-block-big">
-                                        <h4>Tsh <?= number_format($ratio->sum)?></h4>
-                                        <h6><?=$ratio->name?></h6>
+                                        <h4>Tsh <?php // number_format($ratio->sum)?></h4>
+                                        <h6><?php //$ratio->name?></h6>
                                         <i class="icofont icofont-star"></i>
                                     </div>
                                 </div>
-                            <?php } ?>
-                         
+                            <?php // } ?>
+                          -->
 
                         </div>
                     </div>
