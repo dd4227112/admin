1a218598776107efe4377c0782ff73ea5ce5c43d
Albogast <albogastkiyogoma@gmail.com>
2020-11-09 08:45:45 +0300

    Update Stsrem Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -287,8 +287,8 @@ class Kernel extends ConsoleKernel {
             }
             $curl = $this->curlServer($fields, $url);
             $result = json_decode($curl);
-            echo $result->description;
-            if ((strtolower($result->description) == 'success') || $result->description == 'Duplicate Invoice Number') {
+            //echo $result->description;
+            if (isset($result->description) && (strtolower($result->description) == 'success') || $result->description == 'Duplicate Invoice Number') {
 //update invoice no
                 DB::table($invoice->schema_name . '.invoices')
                         ->where('reference', $invoice->reference)->update(['sync' => 1, 'return_message' => $curl, 'push_status' => $push_status, 'updated_at' => 'now()']);
