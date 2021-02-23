8e6fbac6c825f03559eea3271bd1fe174a7201ea
Albogast <albogastkiyogoma@gmail.com>
2021-02-23 16:26:06 +0300

    Upgrade Admin Panel to allow adding user by Excel Upload

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -917,7 +917,7 @@ foreach ($user_permission as $permis) {
 </script>
 <div class="modal fade" id="status-Modal">
 <div class="modal-dialog modal-lg" role="document">
-<form id="add-form" action="userUpload" method="POST" enctype="multipart/form-data">
+<form id="add-form" action="{{ url('users/userUpload') }}" method="POST" enctype="multipart/form-data">
 <?= csrf_field() ?>
       <div class="modal-content">
         <div class="modal-header">
