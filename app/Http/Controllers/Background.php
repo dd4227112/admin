3df1ba1e49031245959a56c734a528bf696f8f00
Ephraim <swillae1@gmail.com>
2021-06-05 08:15:38 +0300

    sms

--- app/Http/Controllers/Background.php
+++ app/Http/Controllers/Background.php
@@ -32,23 +32,23 @@ class Background extends Controller {
     }
 
     public function sendSms() {
-        $messages = DB::select('select * from public.all_sms limit 15');
-        if (!empty($messages)) {
-            foreach ($messages as $sms) {
-                define('API_KEY', $sms->api_key);
-                define('API_SECRET', $sms->api_secret);
-
-                $karibusms = new \karibusms();
-                $karibusms->set_name(strtoupper($sms->schema_name));
-                $karibusms->karibuSMSpro = $sms->type;
-                $result = (object) json_decode($karibusms->send_sms($sms->phone_number, $sms->body));
-                if ($result->success == 1) {
-                    DB::update('update ' . $sms->schema_name . '.sms set status=1 WHERE sms_id=' . $sms->sms_id);
-                } else {
-                    DB::update('update ' . $sms->schema_name . '.sms set status=0 WHERE sms_id=' . $sms->sms_id);
-                }
-            }
-        }
+//        $messages = DB::select('select * from public.all_sms limit 15');
+//        if (!empty($messages)) {
+//            foreach ($messages as $sms) {
+//                define('API_KEY', $sms->api_key);
+//                define('API_SECRET', $sms->api_secret);
+//
+//                $karibusms = new \karibusms();
+//                $karibusms->set_name(strtoupper($sms->schema_name));
+//                $karibusms->karibuSMSpro = $sms->type;
+//                $result = (object) json_decode($karibusms->send_sms($sms->phone_number, $sms->body));
+//                if ($result->success == 1) {
+//                    DB::update('update ' . $sms->schema_name . '.sms set status=1 WHERE sms_id=' . $sms->sms_id);
+//                } else {
+//                    DB::update('update ' . $sms->schema_name . '.sms set status=0 WHERE sms_id=' . $sms->sms_id);
+//                }
+//            }
+//        }
     }
 
     /**
