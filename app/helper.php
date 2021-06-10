494b9175bcc125db31b76cfaaec7c4b4048cecff
Ephraim Swilla <swillae1@gmail.com>
2021-06-10 13:27:26 +0300

    send email & sms remainder to accountant

--- app/helper.php
+++ app/helper.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function custom_date($datatime) {
     $newTZ = new DateTimeZone('America/New_York');
     date_default_timezone_set('America/New_York');
@@ -105,7 +103,6 @@ function number_to_words($number) {
 
 function userAccessRole() {
     $user_id = \Auth::user()->id;
-
     if ((int) $user_id > 0) {
         $user = \App\Models\User::find($user_id);  
         $permission = \App\Models\PermissionRole::where('role_id', $user->role_id)->get();
@@ -115,7 +112,6 @@ function userAccessRole() {
                 array_push($objet, $perm->permission->name);
             }
         }
-       
         return $objet;
     }
 }
--- resources/views/account/invoice/single.blade.php
+++ resources/views/account/invoice/single.blade.php
@@ -233,7 +233,7 @@
                                         <!-- <br/>
                                         <b>If you make a bank deposit, you will have to notify us to activate your account</b> -->
                                         <?php if(isset($diff_in_months)) { ?>
-                                        <?php if((int)$diff_in_months <= 12) { ?>
+                                        <?php if($diff_in_months <= 12) { ?>
                                         <p class="text-muted well well-sm no-shadow">
                                             This is the payment for shulesoft system for <?=$diff_in_months > 1 ? number_to_words($diff_in_months).' months' : number_to_words($diff_in_months).' month'?>. <br>
                                             Thank you for your business. we're glad to serve you
