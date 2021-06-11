c8c560f9f1c36ab4b14d264c5dc0a5c33e6bcce4
Ephraim Swilla <swillae1@gmail.com>
2021-06-11 16:14:42 +0300

    list of schools by admin,associate and zm

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -10,10 +10,14 @@ class Region extends Model {
      * Generated
      */
     protected $table = 'regions';
-    protected $fillable = ['id', 'name', 'country_id', 'created_at', 'updated_at'];
+    protected $fillable = ['id', 'name', 'country_id','refer_zone_id', 'created_at', 'updated_at'];
     
     public function country() {
             return $this->belongsTo(\App\Models\Country::class, 'country_id', 'id')->withDefault(['name' => 'Tanzania']);
         }
+
+    public function zone() {
+         return $this->belongsTo(\App\Models\ReferZone::class, 'refer_zone_id', 'id');
+     }
 
 }
