5029f96e9e1949b3348375dca230fd39b6bd01c9
Ephraim <swillae1@gmail.com>
2021-07-26 11:15:22 +0300

    change db name

--- app/Http/Controllers/Software.php
+++ app/Http/Controllers/Software.php
@@ -579,7 +579,7 @@ ORDER BY c.oid, a.attnum";
         $projects = DB::connection('project')->select("SELECT a.dt_created,a.due_date,a.title,a.message, b.name as project_name, c.name as task_type, a.type_id, d.name as created_by, e.name as assigned_to, a.user_id,a.project_id,a.assign_to, case when a.legend=1 THEN 'New' when a.legend=2 THEN 'Opened' when a.legend=3 THEN 'Closed' when a.legend=4 THEN 'Start' when a.legend=5 THEN 'Resolve' WHEN a.legend=6 THEN 'Modified' END as final_status, 
 CASE 
 WHEN reply_type=4 THEN 'High Priority' ELSE 'Default Priority'
-END as priority, CASE WHEN status=0 then 'Pending' ELSE 'Closed' END as status FROM `Easycases` a JOIN projects b on b.id=a.project_id JOIN types c on c.id=a.type_id JOIN users d on d.id=a.user_id JOIN users
+END as priority, CASE WHEN status=0 then 'Pending' ELSE 'Closed' END as status FROM `easycases` a JOIN projects b on b.id=a.project_id JOIN types c on c.id=a.type_id JOIN users d on d.id=a.user_id JOIN users
  e on e.id=a.assign_to WHERE a.istype=1 " . $and);
 
         $this->data['headers'] = \collect($projects)->first();
