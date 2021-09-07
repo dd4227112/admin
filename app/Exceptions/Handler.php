eda21a8ba1a50f9ec4bac500854fded901302cf2
maombiamos <maombi.amos@shulesoft.com>
2021-09-07 10:28:49 +0300

    fix error log fx

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,10 +42,11 @@ class Handler extends ExceptionHandler {
             'error_instance' => get_class($e),
             'request' => json_encode(request()->all()),
             "schema_name" => 'admin',
-            'created_by' => session('id'),
-            'created_by_table' => session('table')
+            'created_by' => \Auth::user()->id,
         ];
-        if (!preg_match('/ValidatesRequests.php/i', @$e->getTrace()[0]['file']) || !preg_match('/Router.php/i', @$e->getTrace()[0]['file'])) {
+        if (!preg_match('/ValidatesRequests.php/i', @$e->getTrace()[0]['file']) || !preg_match('/Router.php/i', @$e->getTrace()[0]['file']) || 
+            !preg_match('/Authenticate.php/i', @$e->getTrace()[0]['file']) ||  !preg_match('/RouteCollection.php/i', @$e->getTrace()[0]['file']) ||
+            !preg_match('/Pipeline.php/i', @$e->getTrace()[0]['file']) ) {
              DB::table('admin.error_logs')->insert($object);
         }
 
