e0109dcc6e0fbeb52b5afef93311a978680efb3e
amosmaombi <maombibetets@gmail.com>
2021-11-30 11:06:09 +0300

    CRDB school requests

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -1120,7 +1120,8 @@ class Customer extends Controller {
         $this->data['end'] = request('end_date');
         $this->data['schema'] = request()->segment(3);
         $this->data['client'] = strlen($this->data['schema']) > 2 ? DB::table('clients')->where('username', $this->data['schema'])->first() : '';
-        $this->data['schemas'] = DB::select('select username as "table_schema"  from admin.clients where id in (select client_id from admin.payments) or id in (select client_id from admin.standing_orders) or id in (select client_id from admin.client_trials)');
+        // $this->data['schemas'] = DB::select('select username as "table_schema"  from admin.clients where id in (select client_id from admin.payments) or id in (select client_id from admin.standing_orders) or id in (select client_id from admin.client_trials)');
+        $this->data['schemas'] = DB::select('select username as "table_schema"  from admin.clients');
         $this->data['shulesoft_users'] = \App\Models\User::where('status', 1)->whereNotIn('role_id', array(7, 15))->get();
         //check allocation of trainings
         if (strlen($this->data['schema']) > 2) {
