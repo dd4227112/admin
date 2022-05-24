58ca0aa31dbca6fd1824f9bc4bb63e8735f875e6
amosmaombi <maombibetets@gmail.com>
2022-05-24 08:14:29 +0300

    fix onboard process

--- resources/views/sales/customer_success.blade.php
+++ resources/views/sales/customer_success.blade.php
@@ -39,9 +39,8 @@
                                          <h6 class="m-t-10"><?=$trial_code. ' Trial Code' ?></h6>
                                       </div>
                                         <div class="text-right m-t-10">
-
                                              <a class="btn btn-info btn-sm" 
-                                             href="<?= url('https://' . $client->username . '.shulesoft.com/install/site/' . $client->username)?>" target="_blank"> install </a>
+                                             href="<?= url('https://' . $client->username . '.shulesoft.com/install/database/' . $client->username)?>" target="_blank"> install </a>
                                         </div>
                                     </div>
                                 </div>
