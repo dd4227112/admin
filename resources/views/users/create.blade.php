3b364a46bc17504f4fef39e72b840ca56373764f
ShuleSoft <101623298+shulesoftafrica@users.noreply.github.com>
2022-12-14 22:57:36 +0300

    add option to register only basic details and let users submit remaining items

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
-
 @section('content')
-
-    
-       
          <div class="page-header">
             <div class="page-header-title">
                 <h4> Create user</h4>
@@ -22,7 +18,6 @@
                 </ul>
             </div>
         </div> 
-
         <div class="page-body">
             <div class="row">
                 <div class="card">
