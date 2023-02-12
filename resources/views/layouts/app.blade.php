a2da95155cea5aff1751bd6070dcde86081366ab
ShuleSoft <101623298+shulesoftafrica@users.noreply.github.com>
2023-02-12 19:37:00 +0300

    change url to support css of new domain

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<?php $root =  'http://admin.shulesoft.co/public/';
+<?php $root =  'public/';
 $value = \App\Models\UsersSchool::where('user_id',Auth::user()->id)->get();
 //isset($value) ? dd($value) : 'vaaaaaaaa' 
 ?>
