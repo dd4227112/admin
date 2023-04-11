8889641b3c9a4c6af9f5721ed2bf9b5e50a021b9
ShuleSoft <101623298+shulesoftafrica@users.noreply.github.com>
2023-04-11 15:20:32 +0300

    disable materialized view refresh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -114,7 +114,7 @@ class Kernel extends ConsoleKernel {
                 }
             }
         }
-     DB::select('refresh  materialized view  admin.all_sms ');
+     //DB::select('refresh  materialized view  admin.all_sms ');
     }
 
     function beem_sms($phone_number, $message, $schema_ = null) {
