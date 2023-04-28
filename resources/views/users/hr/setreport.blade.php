43a78df77786bbc39cb41069f53e964530d56523
DAVID DANIEL <david.daniel@shulesoft.africa>
2023-04-28 13:05:37 +0300

    Added edit option on user's KPI

--- resources/views/users/hr/setreport.blade.php
+++ resources/views/users/hr/setreport.blade.php
@@ -58,6 +58,7 @@
                                        <td><?= $target->is_derived == 1 ? 'Yes' : 'No' ?></td>
                                         <td class="text-center"> 
                                             <a title="Delete KPI"  href="<?= url('report/deletetarget/' . $target->uuid) ?>"><i class="icofont icofont-trash text-danger"></i></a>
+                                             <a class="edit_KPI" title="Edit KIi" id="<?=$target->uuid?>"><i class="icofont icofont-pencil text-success"></i></a>
                                         </td>
                                     </tr><!--end tr-->   
                                     <?php
@@ -153,6 +154,85 @@
     </div>
 </div>
 
+
+<!-- Edit  Modal content start here -->
+<div class="modal fade" id="group1">
+    <div class="modal-dialog">
+        <form action="<?=base_url('report/edit_kpi')?>" method="post" class="form-horizontal group_form " role="form">
+
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    Edit Key Performance Indicator
+                </div>
+
+                <div class="modal-body">
+
+                    <div class="form-group row">
+
+                        <div class="col-sm-12">
+
+                            <label class="control-label required">KPI Type</label>
+                            <div class="row">
+                                <div class="col-lg-4 offset-3"><input  disabled type="radio" checked="" onmousedown="$('#kpi_not_derived').hide(); $('#kpi_derived').show();" value="1" name="is_derived" class="form-control  " required> Derived</div>
+                                <div class="col-lg-4"> <input disabled type="radio" onmousedown="$('#kpi_not_derived').show(); $('#kpi_derived').hide();"  value="0" name="is_derived" class="form-control " required> Not Derived</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row" id="kpi_not_derived">
+                        <div class="col-sm-12">
+                            <label class="control-label required">KPI Name</label>
+                            <input id="kpi_name" type="text"  name="kpi" class="form-control ">
+                            <span class="col-sm-4 control-label kpi_alert text-danger">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            <label class="control-label required">Target Value</label>
+                            <input id ="target_value" type="text" name="value" class="form-control ">
+                            <span class="col-sm-4 control-label value_alert text-danger">
+                        </div>
+                    </div>
+                    <div class="form-group row" id="kpi_sql">
+                        <div class="col-sm-12">
+                            <label class="control-label required">Custom Query</label>
+                            <textarea rows="5" id="is_derived_sql" type="text"  name="is_derived_sql" class="form-control "></textarea>
+                            <span class="col-sm-4 control-label is_derived_sql text-danger">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            <label class="control-label required">Start Date</label>
+                            <input type="date" id ="start_date" name="start_date" class="form-control " required>
+                            <input type="hidden" id ="uuid" name="uuid" class="form-control " required>
+                            <input type="hidden" id ="is_derived" name="is_derived" class="form-control " required>
+                            
+                            
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            <label class="control-label required">End Date</label>
+                            <input type="date" id="end_date" name="end_date" class="form-control " required>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" >close</button>
+                    <button type="submit" class="btn btn-success">Save</button>
+                </div>
+                <input type="hidden" name="user_sid" value="<?= $user->id ?>">
+                <?= csrf_field() ?>
+            </div>
+        </form>
+    </div>
+</div>
+
+
 <!-- Modal content start here -->
 <div class="modal fade" id="set_key_performance">
     <div class="modal-dialog">
@@ -161,7 +241,7 @@
             <div class="modal-content">
 
                 <div class="modal-header">
-                    Set Key Performances for all staffs
+                    Set Key Performances
                 </div>
 
                 <div class="modal-body">
@@ -203,6 +283,7 @@
         </form>
     </div>
 </div>
+
 <script>
     $(document).ready(function(){
         $('.set_performance').on('submit', function(){
@@ -224,6 +305,51 @@
            }
            
         });
+        $('.edit_KPI').on('click', function() {
+    var id = $(this).attr('id');
+    $.ajax({
+        url: "<?= url('report/edit_kpi/') ?>/" + id,
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            // Load the modal with the response data
+            console.log(response);
+            $('#kpi_name').val(response.kpi);
+            $('#end_date').val(response.end_date);
+            $('#target_value').val(response.value);
+            $('#uuid').val(response.uuid);
+            $('#is_derived').val(response.is_derived);
+            
+            // $('#kpi').val(response.kpi);
+
+            $('#start_date').val(response.start_date);
+            if (response.is_derived == 0) {
+        $("input[value='0'][name='is_derived']").prop("checked", true);
+        $('#kpi_not_derived').show();
+        $('#kpi_sql').hide();
+        $('#is_derived_sql').val('');
+
+        
+        $('#kpi_derived').hide();
+    } else {
+        $("input[value='1'][name='is_derived']").prop("checked", true);
+        $('#kpi_not_derived').hide();
+        $('#kpi_derived').show();
+        $('#kpi_sql').show();
+        $('#is_derived_sql').val(response.is_derived_sql);
+        
+
+    }
+            $('#group1').modal('show');
+           
+        },
+        error: function(xhr, status, error) {
+            // Handle any errors
+            console.log(xhr.responseText);
+        }
+    });
+});
+
     });
 </script>
 @endsection
