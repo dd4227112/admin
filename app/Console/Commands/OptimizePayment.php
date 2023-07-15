4601cd80d84d0941d1b3932dfbbf558da288fc91
ShuleSoft <101623298+shulesoftafrica@users.noreply.github.com>
2023-07-15 23:20:05 +0300

    optimize payment function upgrade to limit schema selection

--- app/Console/Commands/OptimizePayment.php
+++ app/Console/Commands/OptimizePayment.php
@@ -38,17 +38,17 @@ class OptimizePayment extends Command {
      */
     public function handle() {
         $client = DB::table('admin.clients')->where('status', 4)->first(); //We transfer one at a time
-
-        DB::statement('insert into shulesoft.store_students_id (student_id,schema_name)'
-                . 'select student_id,\'' . $client->username . '\' from shulesoft.student where schema_name=\'' . $client->username . '\' and student_id not in (select student_id from shulesoft.store_students_id)');
-
-        $students = DB::table('shulesoft.store_students_id')->where('status', 0)->limit(30)->get();
+        if (!empty($client)) {
+            DB::statement('insert into shulesoft.store_students_id (student_id,schema_name)'
+                    . 'select student_id,\'' . $client->username . '\' from shulesoft.student where schema_name=\'' . $client->username . '\' and student_id not in (select student_id from shulesoft.store_students_id)');
+        }
+        $students = DB::table('shulesoft.store_students_id')->where('status', 0)->limit(10)->get();
         //check if all payments has been uploaded
         foreach ($students as $student) {
             if (!empty($student)) {
                 $student_id = $student->student_id;
                 //temporary hard-coded for motherofmercy schema_name
-                if (DB::SELECT("SELECT * FROM shulesoft.redistribute_student_payments($student_id, '{$client->username}')")) {
+                if (DB::SELECT("SELECT * FROM shulesoft.redistribute_student_payments($student_id, '{$student->schema_name}')")) {
                     $update = ['status' => 1];
                     DB::table('shulesoft.store_students_id')->where('student_id', $student_id)->update($update);
                     Log::error("Payment optimization succes for student with student_id " . $student_id);
