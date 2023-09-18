ea2d0b762b0f06e8b5196ead6fd8619dcb8cd047
DAVID DANIEL <dd4227112@gmail.com>
2023-09-18 12:04:53 +0300

    missing payments kilimani only

--- app/Console/Commands/missingPayment.php
+++ app/Console/Commands/missingPayment.php
@@ -39,10 +39,10 @@ class missingPayment extends Command
     public function handle()
     {
        //find all school that migrated from old version to new version
-        $schemas =DB::select("select distinct username from admin.clients where is_new_version =1 and status =1 and username  !='rightwayschools' and username in (select  DISTINCT table_schema from information_schema.tables where table_type ='BASE TABLE' and table_schema != 'public' )");
-        foreach ($schemas as $key => $schema) { 
-            DB::statement("select shulesoft.transfermissingpayment('".$schema->username."')");
-        }
+        // $schemas =DB::select("select distinct username from admin.clients where is_new_version =1 and status =1 and username  !='rightwayschools' and username in (select  DISTINCT table_schema from information_schema.tables where table_type ='BASE TABLE' and table_schema != 'public' )");
+        // foreach ($schemas as $key => $schema) { 
+            DB::statement("select shulesoft.transfermissingpayment('kilimanischools')");
+        // }
         
         return 0;
     }
