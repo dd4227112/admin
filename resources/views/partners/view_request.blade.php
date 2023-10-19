e25f218f53f0cf4e86f9715a39417a4d8d963ffc
DAVID DANIEL <dd4227112@gmail.com>
2023-10-19 14:25:39 +0300

    fix crdb bank integration request

--- resources/views/partners/view_request.blade.php
+++ resources/views/partners/view_request.blade.php
@@ -232,7 +232,11 @@
 
                                                 if ((int) $client->is_new_version == 1) {
                                                   $integrated = \DB::table('shulesoft.bank_accounts_integrations')->where('id', $request->bank_accounts_integration_id)->where('schema_name', $request->client->username)->first();
-  
+                                                  if (empty($integrated) ){
+                                                    $bank_id = DB::select("select id from shulesoft.bank_accounts where uuid =(select uuid from ".$request->client->username.".bank_accounts where id =".$request->bank_account_id.")")->first();
+                                                    $integrated = \DB::table('shulesoft.bank_accounts_integrations')->where('id', $bank_id)->where('schema_name', $request->client->username)->first();
+                                                  }
+
                                                 }else{
                                                         $integrated = \DB::table($request->client->username . '.bank_accounts_integrations')->where('id', $request->bank_accounts_integration_id)->first();
                                                 
