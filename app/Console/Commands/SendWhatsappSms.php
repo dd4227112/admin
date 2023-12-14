e139bcdc99668656d46a399c275d613a42625a9b
DAVID DANIEL <dd4227112@gmail.com>
2023-12-14 11:35:35 +0300

    send file through whatsapp channel modified

--- app/Console/Commands/SendWhatsappSms.php
+++ app/Console/Commands/SendWhatsappSms.php
@@ -49,7 +49,7 @@ class SendWhatsappSms extends Command
             if (preg_match('/@c.us/i', $message->phone) && strlen($message->phone) < 19) {
                 if (!empty($message->company_file_id)) {
                     $file = \App\Models\CompanyFile::find($message->company_file_id);
-                    $controller->sendMessageFile($message->phone, $message->message, $file->name, $file->path);
+                    $controller->sendMessageFile($message->phone, $file->path, $file->name, $message->message, $file->extension);
                 } else {
                     $controller->sendMessage($message->phone, $message->message);
                 }
