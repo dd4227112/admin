85e29c316ba3dd9a738430c0e538c4a1bba4a7ae
ShuleSoft <101623298+shulesoftafrica@users.noreply.github.com>
2023-12-29 17:50:18 +0300

    quick sms plugin for beem

--- app/Console/Commands/SendQuickSms.php
+++ app/Console/Commands/SendQuickSms.php
@@ -44,43 +44,39 @@ class SendQuickSms extends Command {
             $schema = $schema_->schema_name;
             if ($schema_->schema_name == 'public') {
                 $messages = DB::select("select a.phone_number as phone, a.body  as body, a.sms_id as id, a.sent_from"
-                . " from public.sms a where a.status = 0 and a.type = 1"
-                . " and sent_from not in ('phonesms','phone-sms') order by priority DESC limit 30");
-            } elseif ((int)$schema_->is_new_version == 1) {
+                                . " from public.sms a where a.status = 0 and a.type = 1"
+                                . " and sent_from not in ('phonesms','phone-sms') order by priority DESC limit 30");
+            } elseif ((int) $schema_->is_new_version == 1) {
                 $messages = DB::select("select a.phone_number as phone, a.body  as body, a.sms_id as id, a.sent_from"
-                . " from shulesoft.sms a where a.status = 0 and a.type = 1"
-                . " and sent_from not in ('phonesms','phone-sms') and schema_name='" . $schema . "' order by priority DESC limit 30");
+                                . " from shulesoft.sms a where a.status = 0 and a.type = 1"
+                                . " and sent_from not in ('phonesms','phone-sms') and schema_name='" . $schema . "' order by priority DESC limit 30");
             } else {
                 $messages = DB::select("select a.phone_number as phone, a.body  as body, a.sms_id as id, a.sent_from"
-                . " from " . $schema . ".sms a where a.status = 0 and a.type = 1"
-                . " and sent_from not in ('phonesms','phone-sms') order by priority DESC limit 30");
+                                . " from " . $schema . ".sms a where a.status = 0 and a.type = 1"
+                                . " and sent_from not in ('phonesms','phone-sms') order by priority DESC limit 30");
             }
-          
+
             $total_sms_sent += !empty($messages) ? count($messages) : 0;
 
             if (!empty($messages)) {
                 foreach ($messages as $message) {
-                    
+
                     $beem = $this->beem_sms($message->phone, $message->body, $schema);
-                    
-                   
-                    if ($schema_->schema_name =='public') {
+
+                    if ($schema_->schema_name == 'public') {
                         DB::table("public.sms")->where('sms_id', $message->id)->update([
                             'status' => 1,
                             'return_code' => json_encode($beem),
                             'updated_at' => now()
                         ]);
                     } else {
-                        $final_schema=(int)$schema_->is_new_version==1 ?'shulesoft':$schema;
+                        $final_schema = (int) $schema_->is_new_version == 1 ? 'shulesoft' : $schema;
                         DB::table($final_schema . ".sms")->where('sms_id', $message->id)->update([
                             'status' => 1,
                             'return_code' => json_encode($beem),
                             'updated_at' => now()
                         ]);
                     }
-                    
-                    
-                    
                 }
             }
         }
@@ -89,178 +85,7 @@ class SendQuickSms extends Command {
     }
 
     function beem_sms($phone_number, $message, $schema) {
-        if ($phone_number != '') {
-            $secret_key = 'MDI2ZGVlMWExN2NlNzlkYzUyYWE2NTlhOGE0MjgyMDRmMjFlMDFjODkwYjU2NjA4OTY4NzZlY2Y3NGZjY2Y0Yw==';
-            $api_key = '5e0b7f1a911dd411';
-
-            // $schema = $schema_ == null ? str_replace('.', null, set_schema_name()) : $schema_;
-
-            if ($schema == 'annagamazo') {
-                $sender_name = 'ANNAGAMAZO';
-            } elseif ($schema == 'rahma') {
-                $sender_name = 'RAHMASCHOOL';
-            } elseif ($schema == 'capricorninstitute') {
-                $sender_name = 'CAPRICORN';
-            } elseif ($schema == 'mgutwasec') {
-                $sender_name = 'MGUTWA SECONDARY';
-            }
-            elseif ($schema == 'victoria') {
-                $sender_name = 'VICTORIA';
-            } else {
-                $sender_name = 'SHULESOFT';
-            }
-            
-
-            // The data to send to the API
-            $posthData = array(
-                'source_addr' => $sender_name,
-                'encoding' => 0,
-                'schedule_time' => '',
-                'message' => $message,
-                'recipients' => [array('recipient_id' => '1', 'dest_addr' => str_replace('+', null, $phone_number))]
-            );
-
-            //.... Api url
-            $Url = 'https://apisms.beem.africa/v1/send';
-            // Setup cURL
-            $ch = curl_init($Url);
-            error_reporting(E_ALL);
-            ini_set('display_errors', 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt_array($ch, array(
-                CURLOPT_POST => TRUE,
-                CURLOPT_RETURNTRANSFER => TRUE,
-                CURLOPT_HTTPHEADER => array(
-                    'Authorization:Basic ' . base64_encode("$api_key:$secret_key"),
-                    'Content-Type: application/json'
-                ),
-                CURLOPT_POSTFIELDS => json_encode($posthData)
-            ));
-
-// Send the request
-            $response = curl_exec($ch);
-            // response of the POST request
-            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            $responseBody = json_decode($response);
-            curl_close($ch);
-
-            if ($httpCode >= 200 && $httpCode < 300) {
-                $return = $this->status_code('1701');
-            } else {
-                $return = $this->status_code(1700);
-            }
-
-
-            // Check for errors
-        } else {
-            $return = $this->status_code(1700);
-        }
-
-        return $return;
-    }
-
-    private function status_code($result) {
-
-        switch ($result) {
-            case '1701':
-                $status = array(
-                    'success' => 1,
-                    'message' => 'Message sent successful'
-                );
-                break;
-            case '1702':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid URL Error,one of the parameters was not provided or left blank'
-                );
-                break;
-            case '1703':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid value'
-                );
-                break;
-            case '1704':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid value type'
-                );
-                break;
-            case '1705':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid message'
-                );
-                break;
-            case '1706':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid destination'
-                );
-                break;
-            case '1707':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Invalid Source (Sender)'
-                );
-                break;
-            case '1709':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'User validation failed'
-                );
-                break;
-            case '1710':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Internal error'
-                );
-                break;
-            case '1025':
-                $status = array(
-                    'success' => 0,
-                    'message' => 'Insufficient credit, contact sales@karibusms.com to recharge your account'
-                );
-                break;
-            default :
-                $status = array(
-                    'success' => 0,
-                    'message' => 'No format results specified'
-                );
-                break;
-        }
-        $code = array('code' => $result);
-        $results = array_merge($status, $code);
-
-        return json_encode($results);
-    }
-
-    function countSMS($message): int {
-        $charLength = mb_strlen($message);
-        if ($charLength >= 0 && $charLength <= 160) {
-            return 1;
-        } elseif ($charLength > 160 && $charLength <= 306) {
-            return 2;
-        } elseif ($charLength > 306 && $charLength <= 459) {
-            return 3;
-        } elseif ($charLength > 459 && $charLength <= 612) {
-            return 4;
-        } elseif ($charLength >= 612 && $charLength <= 765) {
-            return 5;
-        } elseif ($charLength > 765 && $charLength <= 918) {
-            return 6;
-        } elseif ($charLength > 918 && $charLength <= 1071) {
-            return 7;
-        } elseif ($charLength > 1071 && $charLength <= 1224) {
-            return 8;
-        } elseif ($charLength > 1224 && $charLength <= 1377) {
-            return 9;
-        } elseif ($charLength > 1377 && $charLength <= 1530) {
-            return 10;
-        } else {
-            return 0;
-        }
+        return new App\Http\Controllers\Plugins\BeemSms($phone_number, $message, $schema);
     }
 
 }
